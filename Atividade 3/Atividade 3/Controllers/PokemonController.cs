4663e6455b12bd9815d58802f95ce6d7e893d2fc
cassiogamarra <cassiogamarra@outlook.com>
2021-10-03 23:48:55 -0300

    Adicionado protótipo da atividade 3

---
+++ Atividade 3/Atividade 3/Controllers/PokemonController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Atividade_3.Models;
+
+namespace Atividade_3.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PokemonController : ControllerBase
+    {
+        public List<Pokemon> Pokemons;
+        [HttpGet]
+        public IEnumerable<Pokemon> Get()
+        { 
+            return Pokemons;
+        }
+        [HttpPost]
+        public string Create([FromBody] Pokemon pokemon)
+        {
+            Pokemons.Add(pokemon);
+            return "Pokemon adicionado com sucesso!";
+        }
+        [HttpDelete("{codigo}")]
+        public string Delete(int codigo) 
+        {
+            for (int i = 0; i < Pokemons.Count; i++) 
+            {
+                if (Pokemons[i].Codigo == codigo) 
+                {
+                    Pokemons.RemoveAt(i);
+                }
+            }
+            return "Pokemon excluído com sucesso!";
+        }
+    }
+}
---
+++ Atividade 3/Atividade 3/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Hosting; 
+using Microsoft.Extensions.Hosting;
+using Atividade_3.Models;
+using System.Collections.Generic;
+
+namespace Atividade_3
+{
+    public class Program
+    { 
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
